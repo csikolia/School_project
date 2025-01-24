c27facb173eeb3817a28e3fedd123b2a5f212455
Gideon Mutai <19432376+gitmuts@users.noreply.github.com>
2025-01-24 12:13:41 +0300

    add cards endpoints

--- src/main/java/com/cs/credentialsscanner/controller/CustomerController.java
+++ src/main/java/com/cs/credentialsscanner/controller/CustomerController.java
@@ -4,8 +4,13 @@ package com.cs.credentialsscanner.controller;
 import com.cs.credentialsscanner.entity.Customer;
 import com.cs.credentialsscanner.service.CustomerService;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,11 @@ public class CustomerController {
   @GetMapping
   public List<Customer> fetchCustomers() {
     return customerService.getCustomers();
+  }
+
+  @PostMapping
+  public ResponseEntity<String> create(@RequestBody Map<String, String> request) {
+
+    return new ResponseEntity<>(customerService.createCustomer(request), HttpStatus.OK);
   }
 }
--- src/main/java/com/cs/credentialsscanner/service/CustomerService.java
+++ src/main/java/com/cs/credentialsscanner/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.cs.credentialsscanner.service;
 import com.cs.credentialsscanner.entity.Customer;
 import com.cs.credentialsscanner.repo.CustomerRepo;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,17 @@ public class CustomerService {
     }
 
     return customers;
+  }
+
+  public String createCustomer(Map<String, String> request){
+    log.info("Received a request to create {}", request);
+    // validate
+    // save in db
+    // email
+    Customer customer = new Customer();
+    customer.setEmail(request.get("email"));
+
+    customerRepo.save(customer);
+    return "Created";
   }
 }
